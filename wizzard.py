e81c587f37a4c848e54b767c8cf2a731125090ce
James Cox <james@actual-experience.com>
2015-05-21 16:15:34 +0100

    Adds delete last.

--- wizzard.py
+++ wizzard.py
@@ -49,18 +49,49 @@ def cli(config, verbose, debug):
 
 
 @cli.command()
+@click.option('--to-self', is_flag=True)
 @click.argument('message', nargs=-1)
 @pass_config
-def tweet(config, message):
+def tweet(config, to_self, message):
+    if not message:
+        raise click.ClickException('Missing message.')  
     if config.verbose:
         click.echo("Tweeting...")
     try:
         tweeter = TwitterAPI()
-        tweeter.tweet(message=' '.join(message))
-    except tweepy.error.TweepError:
+        tweeter.tweet(
+            message=' '.join(message), 
+            to_self=to_self
+        )
+    except tweepy.error.TweepError as e:
+        if config.debug:
+            click.secho(str(e), fg="red")
         click.secho("Failed!", fg="red")
     else:
         click.secho("Sent.", fg="green")
+    if config.verbose:
+        click.echo("Done.")
+
+
+@cli.command()
+@pass_config
+def delete(config):
+    if config.verbose:
+        click.echo("Getting last tweet...")
+    try:
+        tweeter = TwitterAPI()
+        click.secho(
+            tweeter.last_tweet.text.encode('utf-8'),
+            fg="yellow"
+        )
+        click.confirm("Delete?", abort=True)
+        del tweeter.last_tweet
+    except tweepy.error.TweepError as e:
+        if config.debug:
+            click.secho(e.reason, fg="red")
+        click.secho("Failed!", fg="red")
+    else:
+        click.secho("Deleted.", fg="green")
     if config.verbose:
         click.echo("Done.")
 
@@ -72,5 +103,25 @@ class TwitterAPI(object):
         auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
         self.api = tweepy.API(auth)
  
-    def tweet(self, message):
-        self.api.update_status(status=message)
+    def tweet(self, message, to_self=False):
+        if to_self:
+            self.api.update_status(
+                status=message, 
+                in_reply_to_status_id=self.last_tweet.id
+            )
+        else:
+            self.api.update_status(status=message)
+
+    @property
+    def last_tweet(self):
+        if not hasattr(self, '_last_tweet'):
+            self._last_tweet = self.get_last_tweet()
+        return self._last_tweet
+
+    @last_tweet.deleter
+    def last_tweet(self):
+        self.api.destroy_status(self.last_tweet.id)
+        del self._last_tweet
+
+    def get_last_tweet(self):
+        return self.api.user_timeline(count=1)[0]
